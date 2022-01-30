c8085c78153db6b8721e46748b327752c43c710b
visortelle <visortelle@gmail.com>
2022-01-30 15:01:13 +0100

    Create sandbox docker image that exposes terminal and ghci to web

---
+++ sandbox/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# Expose shell terminal
+gotty --port 8000 --permit-write --width 140 --height 80 --term xterm --close-signal 9 bash &
+
+# Expose ghci terminal
+gotty --port 8001 --permit-write --width 140 --height 80 --term xterm --close-signal 9 ghci
